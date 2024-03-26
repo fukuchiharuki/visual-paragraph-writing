f212fc035282e44a01e4d43394c4430357bb36a5
fukuchiharuki <fukuchi.haruki@gmail.com>
2024-03-26 14:52:13 +0900

    feat: パラグラフにアイコン

--- src/view/paragraphtree/ParagraphTreeItem.ts
+++ src/view/paragraphtree/ParagraphTreeItem.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 export default class ParagraphTreeItem extends vscode.TreeItem {
   constructor(
@@ -7,5 +8,8 @@ export default class ParagraphTreeItem extends vscode.TreeItem {
     public readonly collapsibleState: vscode.TreeItemCollapsibleState
   ) {
     super(label, collapsibleState);
+    this.iconPath = (collapsibleState !== vscode.TreeItemCollapsibleState.None)
+      ? path.join(__filename, '..', '..', '..', '..', 'media', 'paragraph-svgrepo-com.svg')
+      : undefined;
   }
 }
